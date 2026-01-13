c8b368d836f4d3a94fc6cba72419d66eca8b121e
Botamedi <botamedi@gmail.com>
2026-01-13 18:16:51 -0300

    Atualização 13012026

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -77,7 +77,7 @@ function getHolidays(year) {
       const { data, error } = await supabase
         .from("Feriado")
         .select("data")
-        .eq("tipo", "national")
+        .in("tipo", ["national", "manual"])
         .gte("data", `${year}-01-01`)
         .lte("data", `${year}-12-31`);
 
